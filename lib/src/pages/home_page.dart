242d1133c6635778d6cef8cef98dd26aa19d81cd
Jose <jjbordonfranco@gmail.com>
2020-02-17 17:03:28 +0000

    botones

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -19,10 +19,10 @@ class HomePage extends StatelessWidget {
                   children: <Widget>[
                     TopAppBar(),
                     _Header(),
+                    BotonesDesplazables(),
                   ],
                   )
                 ),
-
             ] 
           ),
        ),
@@ -38,5 +38,41 @@ class _Header extends StatelessWidget {
       child:Text('Order Coffee', style: TextStyle(fontWeight:FontWeight.bold, fontSize:20))
     );
   }
+}
+
+class BotonesDesplazables extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   
+    return Row(
+      children: <Widget>[
+        SizedBox(width: 15.0),
+        _dibujaBoton('All'),
+        SizedBox(width: 5.0),
+        _dibujaBoton('Coffee'),
+        SizedBox(width: 5.0),
+        _dibujaBoton('Tea'),
+        SizedBox(width: 5.0),
+        _dibujaBoton('Juice'),
+      ],
+    );
+  }
+
+  Widget _dibujaBoton(String texto) {
+    return FlatButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+        side: BorderSide(color: Colors.grey)
+      ),
+      textColor: Colors.grey,
+      child: Text(texto),
+        // color: Colors.transparent,
+      color: Colors.white,
+      onPressed: (){},
+    );
+
+
+  }
+
 }
 
