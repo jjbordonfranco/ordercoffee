3df7cfddd5d0ccea64b9fde1620ecbf756c78ef0
Jose <jjbordonfranco@gmail.com>
2020-02-18 17:15:17 +0000

    botones color activado

--- lib/src/widgets/botones_desplazables.dart
+++ lib/src/widgets/botones_desplazables.dart
@@ -1,36 +1,53 @@
 import 'package:flutter/material.dart';
 
-class BotonesDesplazables extends StatelessWidget{
+class BotonesDesplazables extends StatefulWidget{
   const BotonesDesplazables({Key key}) : super(key : key);
-  
+
+  @override
+  _BotonesDesplazablesState createState() => _BotonesDesplazablesState();
+}
+
+class _BotonesDesplazablesState extends State<BotonesDesplazables> {
+  List<bool> presionado = [true,false,false,false];
+
   @override
   Widget build(BuildContext context) {
   
     return Row(
       children: <Widget>[
         SizedBox(width: 15.0),
-        _dibujaBoton('All'),
+        _dibujaBoton('All',0),
         SizedBox(width: 5.0),
-        _dibujaBoton('Coffee'),
+        _dibujaBoton('Coffee',1),
         SizedBox(width: 5.0),
-        _dibujaBoton('Tea'),
+        _dibujaBoton('Tea',2),
         SizedBox(width: 5.0),
-        _dibujaBoton('Juice'),
+        _dibujaBoton('Juice',3),
       ],
     );
   }
 
-  Widget _dibujaBoton(String texto) {
+  Widget _dibujaBoton(String texto, int pos) {
     return FlatButton(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(5.0),
         side: BorderSide(color: Colors.grey)
       ),
-      textColor: Colors.grey,
+      textColor: presionado[pos] ? Colors.white: Colors.grey,
       child: Text(texto),
         // color: Colors.transparent,
-      color: Colors.white,
-      onPressed: (){},
+      color: presionado[pos] ? Colors.grey : Colors.white,
+      // color: Colors.white,
+      onPressed: (){
+        setState(() {
+          for (var i=0; i<4; i++) {
+            if (i==pos)  
+              presionado[pos] = true;
+            else
+              presionado[i]=false;
+          }
+        });
+      },
     );
 
 
