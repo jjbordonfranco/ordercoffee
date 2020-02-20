c26dc342331add56cb99b772c8905b5327b989b0
Jose <jjbordonfranco@gmail.com>
2020-02-20 11:37:19 +0000

    Lista desplazable de articulos2

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -14,7 +14,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   Widget build(BuildContext context) {
+  final size = MediaQuery.of(context).size;
     return Scaffold(
        body: SafeArea(  //No dibuja zona superior del dispositivo segun el sistema operativo 
           child: Stack(  //permite dibujar widgets apilados
@@ -30,7 +32,7 @@ class _HomePageState extends State<HomePage> {
                     child: BotonesDesplazables(),
                   ),
                   Container(  //muestra todos los artículos desplazables en vertical
-                    height: 400,
+                    height: size.height * 0.55,
                     child: ListView(
                       children: <Widget>[
                         ListaArticulos(),
@@ -45,22 +47,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
-//esta lista se puede utilizar para incluir en children en un ListView. El ListView no se puede incluir en un Column.
-// List<Widget> _botonesDesplazables(){
-//     List<Widget> botones = [];
-
-//     for (var i = 0; i < 10; i++) {
-//       botones.add(
-//         Column(
-//           children: <Widget>[
-//            _dibujaBoton('All'),
-//           ],
-//         ),
- 
-//       );
-//     }
-//     return botones;
-// }
-
--- lib/src/widgets/botones_desplazables.dart
+++ lib/src/widgets/botones_desplazables.dart
@@ -19,7 +19,7 @@ class _BotonesDesplazablesState extends State<BotonesDesplazables> {
   
     return Row(
       children: <Widget>[
-        SizedBox(width: 15.0),
+        SizedBox(width: 15.0), //separador horizontal para los botones
         _dibujaBoton('All',0),
         SizedBox(width: 5.0),
         _dibujaBoton('Coffee',1),
@@ -32,17 +32,19 @@ class _BotonesDesplazablesState extends State<BotonesDesplazables> {
   }
 
   Widget _dibujaBoton(String texto, int pos) {
+    //muestra un botón redondo con un texto. presionado[pos] es un booleano que indica si el botón
+    //en la posicion pos se presiona. Si es así cambia su apariencia y la de los botones no presionados
     return FlatButton(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5.0),
-        side: BorderSide(color: Colors.grey)
+        borderRadius: BorderRadius.circular(5.0), //borde del botón redondeado
+        side: BorderSide(color: Colors.grey)  //color del borde del botón gris
       ),
-      textColor: presionado[pos] ? Colors.white: Colors.grey,
-      child: Text(texto),
+      textColor: presionado[pos] ? Colors.white: Colors.grey, //texto en botón presionado blanco. boton no presionado gris
+      child: Text(texto),  //texto a mostrar
         // color: Colors.transparent,
-      color: presionado[pos] ? Colors.grey : Colors.white,
+      color: presionado[pos] ? Colors.grey : Colors.white, //color de fondo del botón gris si presionado y blanco si no
       // color: Colors.white,
-      onPressed: (){
+      onPressed: (){  // cambia estado del botón que se presiona a true y a false del resto
         setState(() {
           for (var i=0; i<4; i++) {
             if (i==pos)  
